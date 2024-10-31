77bc4f094a1992c557bfadbf539e1d87adf47e7b
Pranav(Xekti) Enugoda <enugodps@mail.uc.edu>
2024-10-31 20:27:23 +0000

    test

--- quizme/ars/qtype/question.py
+++ quizme/ars/qtype/question.py
@@ -1,1 +1,18 @@
 """Module for the base Question class in the Adaptive Review System."""
+
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Any, optional
+import uuid
+
+
+class Question(ABC):
+    def __init(self, id, question, answer, last_asked):
+        self._id = uuid.UUID4
+        self._question = _question
+        self._answer = _answer
+        self._last_asked = datetime.now()
+    
+    @property
+    
+    def id()
